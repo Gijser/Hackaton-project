0672001ffc25406cf21ff2f052dd99d7c75219a5
job mekkelholt <job.mekkelholt@student.graafschapcollege.nl>
2021-05-21 10:47:14 +0200

    bruh

--- functies/functies.php
+++ functies/functies.php
@@ -1,5 +1,6 @@
 <?php
 
+require_once("database.php");
 
 
 function leaderboard(){
@@ -10,6 +11,27 @@ function leaderboard(){
     $db = "lanparty";
     
     $conn = mysqli_connect($dbhost, $dbuser, $dbpass, $db);
-    $query = mysqli_connect()
+    $query = mysqli_connect();
 
 }
+
+function getTopTen(){
+    $dbhost = "localhost";
+    $dbuser = "root";
+    $dbpass = "";
+    $db = "lanparty";
+    
+    $conn = mysqli_connect($dbhost, $dbuser, $dbpass, $db);
+    $query = mysqli_connect();
+
+    $query = "SELECT * FROM speler ORDER BY speler_aantal_punten DESC LIMIT 10";
+
+    $sql = mysqli_query($conn, $query);
+
+    while ($row = $sql->fetch_assoc()) {
+        echo "<tr><td>" . $row["speler_naam"] . "</td>
+        <td>" . $row["speler_aantal_punten"] . " </td>";
+    }
+}
+
+
