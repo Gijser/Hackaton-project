b96f6766b22a87203719fb126d1cc32f9ea8a0c7
Gijser <57718086+Gijser@users.noreply.github.com>
2021-05-21 10:24:15 +0200

    functies

---
+++ functies/functies.php
@@ -0,0 +1,15 @@
+<?php
+
+
+
+function leaderboard(){
+
+    $dbhost = "localhost";
+    $dbuser = "root";
+    $dbpass = "";
+    $db = "lanparty";
+    
+    $conn = mysqli_connect($dbhost, $dbuser, $dbpass, $db);
+    $query = mysqli_connect()
+
+}
